171d46d6aef6a84656e188e16ab549d099d57899
André Albson <106167607+albsondev@users.noreply.github.com>
2024-11-21 09:55:21 -0300

    organizing the navbar header

--- src/components/Layout/Dashboard/Header/Header.tsx
+++ src/components/Layout/Dashboard/Header/Header.tsx
@@ -1,13 +1,34 @@
 import Link from 'next/link'
-import { Container } from 'react-bootstrap'
+import { Container, NavLink } from 'react-bootstrap'
 import HeaderSidebarToggler from '@/components/Layout/Dashboard/Header/HeaderSidebarToggler'
 import HeaderFeaturedNav from '@/components/Layout/Dashboard/Header/HeaderFeaturedNav'
 import HeaderFlagNav from '@/components/Layout/Dashboard/Header/HeaderFlagNav'
-import HeaderProfileNav from '@/components/Layout/Dashboard/Header/HeaderProfileNav'
 import Breadcrumb from '@/components/Layout/Dashboard/Breadcrumb/Breadcrumb'
 import './Header.scss'
+import { getDictionary } from '@/locales/dictionary'
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core'
+import { PropsWithChildren } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPowerOff } from '@fortawesome/free-solid-svg-icons'
+import HeaderLogout from './HeaderLogout'
 
-export default function Header() {
+type ItemWithIconProps = {
+  icon: IconDefinition;
+} & PropsWithChildren
+
+const ItemWithIcon = (props: ItemWithIconProps) => {
+  const { icon, children } = props
+
+  return (
+    <>
+      <FontAwesomeIcon className="me-2" icon={icon} fixedWidth />
+      {children}
+    </>
+  )
+}
+
+export default async function Header() {
+  const dict = await getDictionary()
   return (
     <header className="header sticky-top mb-4 py-2 px-sm-2 border-bottom">
       <Container fluid className="header-navbar d-flex align-items-center px-0">
@@ -24,8 +45,14 @@ export default function Header() {
         <div className="header-nav ms-auto">
           <HeaderFlagNav />
         </div>
-        <div className="header-nav ms-2">
-          <HeaderProfileNav />
+        <div>
+          <HeaderLogout>
+            <Link href="/" passHref legacyBehavior>
+              <NavLink className="p-2">
+                <ItemWithIcon icon={faPowerOff}>{dict.profile.logout}</ItemWithIcon>
+              </NavLink>
+            </Link>
+          </HeaderLogout>
         </div>
       </Container>
       <div className="header-divider border-top my-2 mx-sm-n2" />
