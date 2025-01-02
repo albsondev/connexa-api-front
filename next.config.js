93a4f46ffc3a801db73cbb22e557a69e3c3a392d
André Albson <106167607+albsondev@users.noreply.github.com>
2025-01-02 10:53:45 -0300

    fix configs next + vercel

--- next.config.js
+++ next.config.js
@@ -17,6 +17,14 @@ const nextConfig = {
       },
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
