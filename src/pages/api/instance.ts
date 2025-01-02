06ee621954522e56bba19c2fad984dbc4a825a5e
André Albson <106167607+albsondev@users.noreply.github.com>
2025-01-02 12:28:13 -0300

    teste fix

--- src/pages/api/instance.ts
+++ src/pages/api/instance.ts
@@ -1,28 +1,26 @@
-import { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next'
 import axios from 'axios'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { method } = req
-  const token = req.headers.authorization
-
-  if (!token) {
-    return res.status(403).json({ error: 'No token provided' })
-  }
-
   try {
+    const token = req.headers.authorization // Certifique-se de que o token está presente
+    if (!token) {
+      return res.status(403).json({ error: 'No token provided' })
+    }
+
     const apiResponse = await axios({
-      method,
+      method: req.method, // Repassa o método (GET, POST, PUT, DELETE)
       url: `${process.env.NEXT_PUBLIC_API_URL}/instance`,
       headers: {
         'Content-Type': 'application/json',
-        Authorization: token,
+        Authorization: token, // Aqui o Bearer Token é repassado
       },
-      data: req.body,
+      data: req.body, // Passa o body (se for POST ou PUT)
     })
 
     return res.status(apiResponse.status).json(apiResponse.data)
   } catch (error: any) {
-    console.error('Error in API middleware:', error.response?.data || error.message)
-    return res.status(error.response?.status || 500).json({ error: 'Internal Server Error' })
+    console.error('Erro ao buscar instâncias:', error.response?.data || error.message)
+    return res.status(error.response?.status || 500).json({ error: 'Erro no proxy da API' })
   }
 }
